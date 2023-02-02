2dfe21dd247a8769de95455acac01b822caf3be4
Oksana Kalinina <ksan4ik92@gmail.com>
2023-02-02 18:23:02 +0300

    fix: add genre and img in Books model

--- models/books.js
+++ models/books.js
@@ -13,6 +13,16 @@ const bookSchema = new Schema({
     type: Number,
     required: true
   },
+  img:{
+    type: String,
+    required:true,
+    default:"https://tataev-market.ru/local/templates/tataev-market/assets/images/products/empty-product-img.jpg"
+  },
+  genre:{
+    type: String,
+    required:true,
+    default:"Боевик"
+  }
 
 })
 
