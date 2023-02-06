39b2b80769a34f269869dc6065e7fcf2f9dc5bc7
Oksana Kalinina <ksan4ik92@gmail.com>
2023-02-07 02:42:59 +0300

    fix: grades

--- controller/grades.js
+++ controller/grades.js
@@ -37,7 +37,8 @@ module.exports.create = async function (req, res){
 
 module.exports.getUserGrade = async (req, res) => {
     try{
-        const gradeUser = await Grades.findOne({bookId: req.params.bookId, userId: req.body.id});
+        const gradeUser = await Grades.findOne({bookId: req.params.bookId, userId: req.body.userId});
+        console.log();
         if(gradeUser) res.status(200).json(gradeUser)
         else res.status(404).json({
             "message": "Нет оценки"
